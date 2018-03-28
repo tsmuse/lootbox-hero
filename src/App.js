1d95d5653e586a693af4945c5671fed61e63bcee
ts <tony@notjustprettypictures.com>
2018-03-27 18:09:56 -0700

    very basic routing happening, now more refactoring
    Issues: now that I have routing working I need to seperate some things and figure out where my state is going to actually live, if it can stay where it is or if it needs to change while breaking the pieces out into the pages they actually need to be on.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "normalize.css";
 import Player from "./Player";
 
 
-
+function Home(props){
+    return(
+        <div>
+            <h1>Welcome to the Lootbox Hero Homepage!</h1>
+            <p> 
+                It's not finished...in fact it's not even started! But here are a few places you can
+                go right now!
+            </p>
+            <Link to="/player">Player Page</Link>
+        </div>
+    );
+}
 class App extends Component {
     render(){
         return(
-            <React.Fragment>
-                <Player player={this.props.player} baseAbilities={this.props.baseAbilities}/>
-            </React.Fragment>
+            // <React.Fragment>
+            //     <Player player={this.props.player} baseAbilities={this.props.baseAbilities}/>
+            // </React.Fragment>
+            <Router>
+                <React.Fragment>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/player" render={props => (
+                        <Player player={this.props.player} baseAbilities={this.props.baseAbilities}/>
+                    )} />
+                </React.Fragment>
+            </Router>
         );
         
     }
