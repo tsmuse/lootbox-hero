ff563de1c7e5d2d97c0dfa052b720dd4ed95c9c4
ts <tony@notjustprettypictures.com>
2018-04-16 18:00:57 -0700

    BROKEN!! but not as broken
    Soo....ejecting didn't work. It broke A LOT of other stuff. I had to create a new create-react-app and move stuff over, so I'm now back to where I was at the beginning of all of those, unable to get the totally not modular Phaser-CE libary to load using create-react-app's webpack setup. I may have to bite the bullet and live on the bleeding edge with Phaser3, which doesn't use PIXI but is modular...

--- src/components/WorkGames/WorkGames.js
+++ src/components/WorkGames/WorkGames.js
@@ -1,12 +1,13 @@
+/* global Phaser, p2, PIXI */
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 // import * as PIXI from "pixi.js";
 // import { setupTreasureHunter, treasureHunter } from "../../microgames/TreasureHunter";
 // import { setupPlaceholder, placeholderGame } from "../../microgames/PlaceholderGame";
 // import Phaser from "phaser-ce";
-import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
-import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
-import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
+// import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
+// import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
+// import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
 import { preloadTreasureHunterPhaser, createTreasureHunterPhaser, TreasureHunter } 
     from "../../microgames/TreasureHunterPhaser";
 
