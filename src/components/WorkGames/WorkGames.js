1591bf64a8deb2966864b78eb1c98accdabfed8e
ts <tony@notjustprettypictures.com>
2018-04-17 18:24:00 -0700

    scene switching sorta working
    It's loading my one game, but it's not loading it multiple times...not sure if that's because it's the exact same game or what. Everything else (Score updating, the game over scene) seems to be working

--- src/components/WorkGames/WorkGames.js
+++ src/components/WorkGames/WorkGames.js
@@ -12,8 +12,9 @@ import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js'
 
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
-import { preloadTreasureHunterPhaser, createTreasureHunterPhaser, TreasureHunter } 
-    from "../../microgames/TreasureHunterPhaser";
+import { rolldice } from "../../Helpers";
+import TreasureHunterState from "../../microgames/TreasureHunterPhaser";
+
 
 class WorkGames extends Component {
     constructor(props) {
@@ -21,21 +22,46 @@ class WorkGames extends Component {
         this.state = {
             gameOver : false
         };
-        this.shiftList = this.props.shiftList;
-        this.scenesToPlay = [];
-        this.sceneTimer = 5;
+        this.shiftLength = 2; // need to figure out where this should live
+        this.workGamesLibrary = [
+            { name: "TreasureHunter", state: TreasureHunterState, value: 10 }
+        ];
+        this.sceneCounter = -1;
         this.scores = [];
-        this.setupGame = this.setupGame.bind(this);
+        this.phaserConfig = { 
+            width: 512,
+            height: 512,
+            renderer: Phaser.AUTO,
+            antialias: false,
+            parent: this.container
+            // state: {
+            //     preload: this.preloadWorkIntro, 
+            //     create: this.createWorkIntro, 
+            //     update: this.updateWorkIntro
+            // }
+
+        };
         // define the scenes for each microgame
         this.placeholderScenes = [];
         // bind the functions from each game module?
+        this.switchScene = this.switchScene.bind(this);
+        this.gameOver = this.gameOver.bind(this);
+        
 
     }
     componentDidMount() {
-        this.game = new Phaser.Game(512, 512, Phaser.AUTO, this.container, 
-            { preload: this.preload, create: this.create, update: this.update }
-        );
+        this.sceneList = this.generateWorkList();
+        this.game = new Phaser.Game(this.phaserConfig);
+        this.game.scores = [];
+        this.sceneList.forEach((scene)=>{
+            this.game.state.add(scene.name, scene.state);
+        });
+        // this.game.state.add("treasureHunter", TreasureHunterState, true);
+        this.game.state.add("switch", {create: this.switchScene});
+        this.game.state.add("gameOver", {create:this.gameOver});
+        this.game.state.start("switch");
     }
+
     componentWillUnmount(){
         // this.game.destroy(true);
         // loader.reset();
@@ -53,15 +79,86 @@ class WorkGames extends Component {
         }
 
     }
-    preload(){
-        preloadTreasureHunterPhaser(this.game);
+    switchScene(game) {
+        console.log("switch!");
+        this.sceneCounter += 1;
+        if(this.sceneCounter < this.sceneList.length){
+            let nextScene = this.sceneList[this.sceneCounter];
+            game.state.start(nextScene.name);
+        }
+        else{
+            game.state.start("gameOver");
+        }
+
+        // if (nextScene === this.treasureHunter) {
+        //     this.treasureHunterScene.visible = true;
+        //     this.currentScene = this.treasureHunterScene;
+        //     this.gameState = this.treasureHunter;
+        // }
+        // else {
+        //     // this is convoluted because I don't have unique games for everything else
+        //     let placeholder = this.placeholderScenes.shift();
+        //     placeholder.visible = true;
+        //     this.currentScene = placeholder;
+        //     this.gameState = this.placeholderGame;
+        // }
+
     }
-    create(){
-        createTreasureHunterPhaser(this.game);
-        this.gameState = TreasureHunter;
+    generateWorkList() {
+        var sceneList = [];
+        for (let i = 0; i < this.shiftLength; i++) {
+            let scene = this.workGamesLibrary[rolldice(1) - 1];
+                
+            // check for duplicate rolls and keep rolling until we don't have a dupe
+            // while (keys.length > 0 && keys.indexOf(game.name) !== -1) {
+            //     game = this.workGamesLibrary[rolldice(10) - 1];
+            // }
+            sceneList.push(scene);
+        }
+        // // for testing/debugging a specific game
+        // gameList = { "workGame10": this.workGamesLibrary[9] };
+
+        return sceneList;
+
     }
-    update(){
-        this.gameState();
+    // preload(){
+    //     // saving this sound stuff for later
+    //     // this.game.load.audio(
+    //     //     'boden', 
+    //     //     ["gameAssets/audio/bodenstaendig_2000_in_rock_4bit.mp3",
+    //     //         "assets/audio/bodenstaendig_2000_in_rock_4bit.ogg"]);
+
+    //     preloadTreasureHunterPhaser(this.game);
+    // }
+    // create(){
+    //    // createTreasureHunterPhaser(this.game);
+    //     //this.gameState = TreasureHunter;
+    //     // saving this sound stuff for later
+    //     // this.music = this.game.add.audio('boden');
+    //     // this.music.play();
+    // }
+    // update(){
+    //     this.gameState(this.game);
+    // }
+    gameOver(game){
+        game.add.text(
+            game.world.width /2, 
+            game.world.height/2, 
+            "game over mate", 
+            {fill: "#ffffff", align: "center"}
+        );
+        this.scoreGame(game.scores);
+        game.time.events.add(3000, this.end, this);
+    }
+    scoreGame(scores) {
+        let cashEarned = 0;
+        scores.forEach((success, index) => {
+            if (success) {
+                console.log(`cash + ${this.sceneList[index].value}`);
+                cashEarned += this.sceneList[index].value;
+            }
+        }, this);
+        this.props.workCompleteHandler(cashEarned);
     }
 
     // Old setup method. Most of this logic needs to move into the new one
@@ -100,33 +197,8 @@ class WorkGames extends Component {
         this.game.ticker.add(delta => this.gameLoop(delta));
     }
 
-    switchGame(){
-        console.log("switch!");
-        let nextScene = this.scenesToPlay.shift();
-        if(nextScene === this.treasureHunter){
-            this.treasureHunterScene.visible = true;
-            this.currentScene = this.treasureHunterScene;
-            this.gameState = this.treasureHunter;
-        }
-        else{
-            // this is convoluted because I don't have unique games for everything else
-            let placeholder = this.placeholderScenes.shift();
-            placeholder.visible = true;
-            this.currentScene = placeholder;
-            this.gameState = this.placeholderGame;
-        }
-        
-    }
-    scoreGame(){
-        let cashEarned = 0;
-        this.scores.forEach((success, index) =>{
-            if(success){
-                console.log(`cash + ${Object.entries(this.shiftList)[index][1].value}`);
-                cashEarned += Object.entries(this.shiftList)[index][1].value;
-            }
-        }, this);
-        this.props.workCompleteHandler(cashEarned);
-    }
+    
+    
     gameComplete(){
         if (this.scenesToPlay.length > 0) {
             this.currentScene.visible = false;
--- src/microgames/TreasureHunterPhaser.js
+++ src/microgames/TreasureHunterPhaser.js
@@ -1,6 +1,24 @@
-import Phaser from "phaser-ce";
+/* global Phaser, p2, PIXI */
+import { contain, randomInt } from "../Helpers";
 
-export function preloadTreasureHunterPhaser(game){
+var dungeon,
+    walls,
+    door,
+    explorer,
+    treasure,
+    blobs,
+    healthBar,
+    cursors;
+
+    var TreasureHunterState = {
+    preload: preloadTreasureHunterPhaser,
+    create: createTreasureHunterPhaser,
+    update: treasureHunter,
+}
+
+export default TreasureHunterState;
+
+function preloadTreasureHunterPhaser(game){
     game.load.atlasJSONHash(
         "thAtlas", 
         "gameAssets/sprites/treasureHunter.png",
@@ -8,10 +26,120 @@ export function preloadTreasureHunterPhaser(game){
     );
 }
 
-export function createTreasureHunterPhaser(game){
-    var dungeon = game.add.sprite(0,0,"thAtlas","dungeon.png");
+function createTreasureHunterPhaser(game){
+    dungeon = game.add.sprite(0,0,"thAtlas","dungeon.png");
+    
+    door = game.add.sprite(32, 0, "thAtlas", "door.png");
+    game.physics.arcade.enable(door);
+    
+    explorer = game.add.sprite(68, game.world.height / 2, "thAtlas", "explorer.png");
+    explorer.y = explorer.y - (explorer.height/2);
+    game.physics.arcade.enable(explorer);
+    
+    treasure = game.add.sprite(0,0,"thAtlas","treasure.png");
+    treasure.x = game.world.width - treasure.width - 48;
+    treasure.y = game.world.height / 2 - treasure.height / 2;
+    game.physics.arcade.enable(treasure);
+
+    var numBlobs = 6,
+        spacing = 48,
+        xOffset = 150,
+        speed = 200,
+        direction = 1;
+
+    blobs = game.add.group();
+    blobs.enableBody = true;
+    for(let i = 0; i < numBlobs; i++){
+        let blob = blobs.create(0,0,"thAtlas","blob.png");
+        blob.x = spacing * i + xOffset;
+        blob.y = randomInt(0, game.world.height - blob.height);
+        blob.body.velocity.y = speed * direction;
+        // blob.body.collideWorldBounds = true;
+        direction *= -1;
+    }
+
+    healthBar = game.add.group();
+    healthBar.x = game.world.width - 170;
+    healthBar.y = 4;
+
+    //Create black background rectangle
+    var innerBar = new Phaser.Graphics(game);
+    innerBar.beginFill(0x000000);
+    innerBar.drawRect(0, 0, 128, 8);
+    innerBar.endFill();
+    healthBar.addChild(innerBar);
+
+    //Create red front rectangle
+    var outerBar = new Phaser.Graphics(game);
+    outerBar.beginFill(0xFF3300);
+    outerBar.drawRect(0, 0, 128, 8);
+    outerBar.endFill();
+    healthBar.addChild(outerBar);
+    healthBar.outer = outerBar;
+
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
-export function TreasureHunter(){
+function treasureHunter(game){
+    // check the explorer is inside the walls
+    contain(explorer, { x: 28, y: 10, width: 488, height: 480 });
+    explorer.alpha = 1;
 
+    // keep the blobs moving
+    blobs.forEach(blob => {
+        let blobHitsWall = contain(blob, { x: 28, y: 10, width: 488, height: 480 });
+
+        if(blobHitsWall === "top"){
+            blob.y += 5;
+            blob.body.velocity.y *= -1;
+
+        }
+        else if(blobHitsWall === "bottom"){
+            blob.y -= 5;
+            blob.body.velocity.y *= -1;
+        }
+    });
+    // check if explorer is hit by a blob
+   game.physics.arcade.overlap(explorer, blobs,(explorer,blob) =>{
+        explorer.alpha = 0.5;
+        healthBar.outer.width -= 1;
+    });
+
+    // check if explorer is touching the treasure
+    game.physics.arcade.overlap(explorer, treasure, (explorer, treasure) => {
+        treasure.x = explorer.x + 8;
+        treasure.y = explorer.y + 8;
+    });
+
+    // check explorer health, if out of health end game
+    if(healthBar.outer.width < 0){
+        game.scores.push(false);
+        game.state.start("gameOver");
+    }
+
+    // check if the treasure made it to the door
+    game.physics.arcade.overlap(treasure, door, (treasure,door) => {
+        game.scores.push(true);
+        game.state.start("gameOver");
+    });
+
+    // move the explorer around based on arrow keys
+    explorer.body.velocity.x = 0;
+    explorer.body.velocity.y = 0;
+    if(cursors.left.isDown){
+        explorer.body.velocity.x = -300;
+        explorer.body.velocity.y = 0;
+    }
+    else if(cursors.right.isDown){
+        explorer.body.velocity.x = 300;
+        explorer.body.velocity.y = 0;
+    }
+    else if(cursors.up.isDown){
+        explorer.body.velocity.x = 0;
+        explorer.body.velocity.y = -300;
+    }
+    else if(cursors.down.isDown){
+        explorer.body.velocity.x = 0;
+        explorer.body.velocity.y = 300;
+    }
 }
