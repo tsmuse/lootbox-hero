415406ff1c1f310711577e2b7f647cf9a8c4b5f7
ts <tony@notjustprettypictures.com>
2018-04-30 17:06:47 -0700

    More experiments in animating the generation of lootboxes
    I created what might be the worst looking treasure chest sprite in the history of drawing to allow me to play with the CSS on this. Still not finished but I think I have the way forward figured out.  TODOs in the code, hiopefully this will be a quick bit to finish up tomorrow.

--- src/components/Lootboxes/Lootboxes.js
+++ src/components/Lootboxes/Lootboxes.js
@@ -6,10 +6,12 @@ class Lootboxes extends Component {
     constructor(props){
         super(props);
         this.handleOpenBoxes = this.handleOpenBoxes.bind(this);
+        this.handleOpenBox = this.handleOpenBox.bind(this);
         // this.lootID = generateLootIDs();
         this.state = {
-            "isLoaded" : false,
-            "lootTable" : {}
+            isLoaded : false,
+            lootTable : {},
+            boxesOpened : []
         };
     }
     
@@ -28,6 +30,10 @@ class Lootboxes extends Component {
     handleOpenBoxes() {
         const lootbox = generateBoxes();
         this.props.lootboxChangeHandler(lootbox);
+    }
+
+    handleOpenBox(evt) {
+        console.log(evt);
     }
 
     render(){
@@ -68,23 +74,34 @@ class Lootboxes extends Component {
                             Buy Box
                         </button>
                     </div>
+                    {/* 
+                        TODO: give each box an id that is the index representing the box in the
+                        boxeOpened array in state, set the unopened-box class conditional to 
+                        the value in state, pass in random values for animation-iteration-count
+                        and background-position-y here, so they will be regenerated each time instead
+                        of when the Sass is compiled like they are right now.    
+                    */}
                     <ul className="loot-list">
                         {boxToRender.map(function (item, index) {
                             return (
-                                <li className="loot-item unopenedBox" key={`item_${index}`}>
-                                    <h2 className="item-label">{`Item ${index + 1}`}</h2>
-                                    <div className="item-card">
-                                        <h3 className="item-name">{item.name}</h3>
-                                        <p className="item-desc">{item.desc}</p>
-                                    </div>
+                                <li className={`loot-item unopenedBox-${index}`} key={`item_${index}`}>
+                                    <a className="loot-item-button" onClick={this.handleOpenBox}>
+                                        <h2 className="item-label">{`Item ${index + 1}`}</h2>
+                                        <div className="item-card">
+                                            <h3 className="item-name">{item.name}</h3>
+                                            <p className="item-desc">{item.desc}</p>
+                                        </div>
+                                    </a>
                                 </li>
                             );
-                        })}
+                        }, this)}
                     </ul>
                 </section>
             );
         }
     }
+
+    
 }
 
 export default Lootboxes;
